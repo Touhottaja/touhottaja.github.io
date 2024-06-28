3fe486abc4524faceafa7775fb55164f4dd9faea
Touhottaja <touhottaja@icloud.com>
2024-06-28 16:51:04 +0300

    Add nvidia driver install/update script

---
+++ nvidia_drivers.sh
@@ -0,0 +1,5 @@
+sudo apt install nvidia-driver-535-open
+sudo apt install --reinstall libgl1-mesa-glx libglx-mesa0
+sudo dpkg-reconfigure xserver-xorg
+sudo apt install libglu1-mesa freeglut3-dev mesa-utils
+sudo reboot
